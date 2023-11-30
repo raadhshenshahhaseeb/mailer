4c06cd84317ae72cdf42bada28dd6dbf12e346a6
Haseeb Raja <has33braja@gmail.com>
2023-11-30 11:45:06 +0500

    chore: port for heroku

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/sirupsen/logrus"
@@ -36,14 +37,21 @@ func Init() error {
 }
 
 func (c *Services) startServer() {
-	serverConf := c.Config.GetConfig().Server
-	address := serverConf.Host + serverConf.PORT
+	var port string
+	if os.Getenv("PORT") != "" {
+		// Use the PORT environment variable provided by Heroku
+		port = ":" + os.Getenv("PORT")
+	} else {
+		// Fallback to a default port, useful when running the application locally
+		port = ":8080" // or any default port you prefer
+	}
 
+	address := port
 	c.logger.Info("Starting Server at:", address)
 
 	err := http.ListenAndServe(address, c.Api.GetRouter())
 	if err != nil {
-		c.logger.Error("error starting server at ", address, " with error: ", err)
+		c.logger.Error("Error starting server at ", address, " with error: ", err)
 		panic(err)
 	}
 }
